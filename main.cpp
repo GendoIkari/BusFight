b0338f2d12ee989460621a00704b438d59ccc3ee
Daniele Maccioni <gendoikari@develer.com>
2023-04-10 14:41:59 +0200

    Use Better Icons
    Add an application icon and improving the other ones.

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ int main(int argc, char *argv[])
 {
     QApplication::setStyle("Fusion");
     QApplication::setDesktopSettingsAware(false);
+    QApplication::setWindowIcon(QIcon(":/assets/icon.svg"));
 
     QApplication app(argc, argv);
     app.setApplicationName("Bus Fight");
